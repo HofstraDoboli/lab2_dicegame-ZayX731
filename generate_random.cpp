291beff4545ca9f243df1c117d8f05cb7b63eec5
ZayX731 <Zayfootballplayer@gmail.com>
2021-02-16 20:47:03 +0000

    First one of the day First problem

--- generate_random.cpp
+++ generate_random.cpp
@@ -30,26 +30,26 @@ int main()
     for (int i = 0; i < 10; i ++)
         cout << rand() << '\t';
     cout << "\nQ1: What is the output of rand(). Why? " << endl;
-    answer = "" ; // 1. Write your answer to Q1 in between " "
+    answer = "All numbers from 0-5 outputted because it randomizes it because its between 0 and randMax" ; // 1. Write your answer to Q1 in between " "
     cout << "Answer Q1: " << answer << endl;
 
     cout << endl;
     for (int i = 0; i < 10; i ++)
         cout << rand()%5 << '\t';
     cout << "\nQ2: What is the output of rand()%5. Why? " << endl;
-    answer = "" ; // 2. Write your answer to Q2 in between " "
+    answer = "% means modulo and the answer that will be provided will be the remainder of the division beteen two numbers " ; // 2. Write your answer to Q2 in between " "
     cout << "Answer Q2: " << answer << endl;
 
     cout << endl;
     for (int i = 0; i < 10; i ++)
         cout << rand()%5+3 << '\t';
     cout << "\nQ3: What is the output of rand()%5+3. Why?" << endl;
-    answer = "" ; // 3. Write your answer to Q3 in between " "
+    answer = "% means modulo and the answer that will be provided will be the remainder of the division beteen two numbers between and then adding 3 to the value"  ; // 3. Write your answer to Q3 in between  
     cout << "Answer Q3: " << answer << endl;
 
     // Comment the line srand(time(NULL)); compile and run the program twice
     cout << "\nQ4: What is the output of the program when srand(time(NULL)) is removed. Why" << endl;
-    answer = "" ; // 4. Write your answer to Q4 in between " "
+    answer = "It doesnt randomize when you run it the second time. When you get rid of it you use the same sequence of numbers" ; // 4. Write your answer to Q4 in between " "
     cout << "Answer Q4: " << answer << endl;
 
     // 5. Change the rand() expression below to generate a random number 
@@ -58,10 +58,11 @@ int main()
     bool is_error = false; 
     for (int i = 0; i < 50; i ++)
     {
-        int dice_output = rand(); //change this expression so dice_output has a value between 1 and 6
+        int dice_output = rand()%6+1; //change this expression so dice_output has a value between 1 and 6
         if (dice_output < 1 || dice_output > 6)
         {
             cout << "Error: your code does not generate # between 1 and 6" << endl;
+            //cout<<dice_output<<endl;
             is_error = true;
             break;  // exits the loop
         }
